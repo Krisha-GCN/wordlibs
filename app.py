8655611d2718a65bb617b4aa274f95d207059be7
breadpandedpan <krisha.agarwal@csedge.org>
2024-07-18 13:45:18 -0700

    added python code

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 
 @app.route('/completed', methods=["GET","POST"])
 def completed():
-
+    if request.method == "POST":
+        return render_template("completed.html", adj1 = adj1)
+    return render_template("madlibs.html", url=url_for("madlibs"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
