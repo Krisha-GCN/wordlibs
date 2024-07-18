2d8229e24a7c497b1f3fd5b5674457c2d83776ae
breadpandedpan <krisha.agarwal@csedge.org>
2024-07-18 13:26:00 -0700

    added completed.html

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 
+
+@app.route('/completed', methods=["GET","POST"])
+def completed():
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
